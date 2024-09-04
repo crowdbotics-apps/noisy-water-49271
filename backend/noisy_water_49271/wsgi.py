63bacad77ba24c7e4582f1700b7cf0eadf331c9f
Crowdbotics <team@crowdbotics.com>
2024-09-04 07:15:13 +0000

    Initial commit

---
+++ backend/noisy_water_49271/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for noisy_water_49271 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "noisy_water_49271.settings"
+)
+
+application = get_wsgi_application()
